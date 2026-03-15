7f0a7df37ff39c8457ffa765a849c865e4a3392f
Park Ian Co <opensource@parkian.co>
2026-03-15 05:25:06 -0400

    Harmonize .asd system definitions, metadata, and license

--- src/impl.lisp
+++ src/impl.lisp
@@ -1,7 +1,7 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
 ;; SPDX-License-Identifier: Apache-2.0
 
-(in-package :cl_bls_signatures)
+(in-package #:cl-bls-signatures)
 
 (defun init ()
   "Initialize module."
